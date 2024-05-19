5af2a18798f68ca3d2dfe5e99682daf792a69070
dariberrie <darigoldman@gmail.com>
2024-05-19 16:52:28 +0200

    adjust user validations; improve specs; add assignments spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,46 +2,46 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation tests' do
+    let(:valid_attributes) do
+      {
+        first_name: "Appalito",
+        last_name: "Goldman",
+        email: "appalito@example.com",
+        address: "10 Rue Stephenson, Paris",
+        password: "Password0!"
+      }
+    end
+
     it 'ensures first name presence' do
-      user = User.new(last_name: "Johnson", email: "john@johnson.com").save
+      user = User.new(valid_attributes.except(:first_name)).save
       expect(user).to eq(false)
     end
 
     it 'ensures last name presence' do
-      user = User.new(first_name: "John", email: "john@johnson.com").save
+      user = User.new(valid_attributes.except(:last_name)).save
       expect(user).to eq(false)
     end
 
     it 'ensures email presence' do
-      user = User.new(first_name: "John", last_name: "Johnson").save
-      expect(user).to eq(false)
-    end
-
-    it 'ensures address presence' do
-      user = User.new(first_name: "John", last_name: "Johnson").save
+      user = User.new(valid_attributes.except(:email)).save
       expect(user).to eq(false)
     end
 
     it 'should save successfully' do
-      user = User.new(first_name: "John", last_name: "Johnson", email: "john@johnson.com", 
-                      address: "10 Rue Stephenson, Paris", password: "Password0!").save
+      user = User.new(valid_attributes).save
       expect(user).to eq(true)
     end
-
   end
 
-  context 'scope tests' do 
-
+  context 'scope tests' do
     it 'should return users with resident role' do
-      User.new(first_name: "John", last_name: "Johnson", email: "john@johnson.com", 
-               address: "10 Rue Stephenson, Paris", password: "Password0!").save
-      expect(User.resident_users.size).to eq(1)
+      expect { User.create(first_name: "Appa", last_name: "Goldman", email: "appa_resident@example.com",
+        address: "10 Rue Stephenson, Paris", password: "Password0!") }.to change { User.resident_users.count}.by(1)
     end
 
     it 'should return users with worker role' do
-      user = User.new(first_name: "John", last_name: "Johnson", email: "john@johnson.com", 
-               address: "10 Rue Stephenson, Paris", password: "Password0!", role: 1).save   
-      expect(User.worker_users.size).to eq(1)
+      expect { User.create(first_name: "Bappa", last_name: "Goldman", email: "bappa_worker@example.com",
+        address: "10 Rue Stephenson, Paris", password: "Password0!", role: "worker") }. to change { User.worker_users.count}.by(1)
     end
   end
 end
